0259cec1b7d3176066ffbac61ebbd800417e7221
judycpc <abc0952753111@gmail.com>
2022-05-04 14:40:18 +0800

    update login api & create logout api

--- backend/README.md
+++ backend/README.md
@@ -15,13 +15,49 @@ Step 5: Go to `/backend/db.js` & change the password into your Postgres password
     "password": "11111111"
 }
 ```
-#### Response Data: Text
+#### Response Data: JSON
 ```
 Case 1: Username does not exist.
-Case 2: Wrong password.
-Case 3: Login successfully.
-```
+{
+    "message": "Username does not exist.",
+    "username": "username"
+}
 
+Case 2: Wrong password.
+{
+    "message": "Wrong password.",
+    "username": "userONE"
+}
+
+Case 3: Login successfully.
+{
+    "message": "Login successfully.",
+    "username": "userONE",
+    "login_id": "03c940bc-83b8-4142-88a8-da555661c346"
+}
+```
+Note: `login_id` needs to be recorded at client side.
+
+### 2. /logout
+#### Method: POST
+#### Request Data: JSON
+```
+{
+    "login_id": "03c940bc-83b8-4142-88a8-da555661c346"
+}
+```
+#### Request Data: JSON
+```
+Case 1: login_id is not in the online list.
+{
+    "message": "login_id error"
+}
+
+Case 2: Logout successfully.
+{
+    "message": "Logout successfully."
+}
+```
 # Activate Server
 Step 1: Run `npm install` if it's the first time to activate the server.  
 Step 2: Run `npm start`.  
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const pool = require("../db");
+const { v4: uuidv4 } = require('uuid');
+
+let online = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,18 +13,41 @@ router.get('/', function(req, res, next) {
 /* POST login info. */
 router.post('/login', async (req, res) => {
   try {
-      const {username, password} = req.body;
-      const user = await pool.query('SELECT * FROM "WormGym".user_info WHERE username = $1', [username]);
-      
-      if (user.rows[0] === undefined) {
-          res.send('Username does not exist.');
-      } else if (password !== user.rows[0].password) {
-          res.send('Wrong password.');
-      } else {
-          res.send('Login successfully.');
-      }
+    const {username, password} = req.body;
+    const user = await pool.query('SELECT * FROM "WormGym".user_info WHERE username = $1', [username]);
+    let message = "";
+
+    if (user.rows[0] === undefined) {
+      message = 'Username does not exist.';
+      res.send({message, username});
+    } else if (password !== user.rows[0].password) {
+      message = 'Wrong password.';
+      res.send({message, username});
+    } else {
+      const login_id = uuidv4();
+      online.push({username, login_id});
+
+      message = 'Login successfully.';
+      res.send({message, username, login_id});
+    }
   } catch (err) {
-      console.error(err.message);
+    console.error(err.message);
+  }
+});
+
+/* POST logout */
+router.post('/logout', (req, res) => {
+  try {
+    const {login_id} = req.body;
+    const foundUser = online.find((user) => user.login_id === login_id);
+    if (foundUser === undefined) {
+      res.send({"message": "login_id error"});
+    } else {
+      online = online.filter((user) => user.login_id !== login_id);
+      res.send({"message": "Logout successfully."});
+    }
+  } catch (err) {
+    console.error(err.message);
   }
 });
 
