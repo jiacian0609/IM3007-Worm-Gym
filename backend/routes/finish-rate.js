e3500f0a05f6c0344b982ff689205845a9a6565c
judycpc <abc0952753111@gmail.com>
2022-05-14 00:21:03 +0800

    Update finish-rate.js

--- backend/routes/finish-rate.js
+++ backend/routes/finish-rate.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require("jsonwebtoken");
 const pool = require("../db");
 
-router.get('/:username', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         // get user_id
-        const {username} = req.params;
-        const user =  await pool.query('SELECT * FROM "WormGym".user_info WHERE username = $1', [username]);
-        const user_id = user.rows[0].user_id;
+        var JWT = req.headers.authorization
+        var payload = jwt.verify(JWT, "b7b16ad9db0ca7c5705cba37840e4ec310740c62beea61cfd9bdcee0720797a6c8bb1b3ffc0d781601fb77dbdaa899acfd08ac560aec19f2d18bb3b6e25beb7a");
+        var user_id = payload.Uid;
 
         // get all month
         const month = await pool.query(`SELECT EXTRACT(YEAR FROM month_trunc) AS year, EXTRACT(MONTH FROM month_trunc) as month
