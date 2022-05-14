86b1ad989b793a25e056177ad82890c194e00131
judycpc <abc0952753111@gmail.com>
2022-05-14 18:42:26 +0800

    Update finish-rate.js

--- backend/routes/finish-rate.js
+++ backend/routes/finish-rate.js
@@ -9,6 +9,57 @@ router.get('/', async (req, res) => {
         var JWT = req.headers.authorization
         var payload = jwt.verify(JWT, "b7b16ad9db0ca7c5705cba37840e4ec310740c62beea61cfd9bdcee0720797a6c8bb1b3ffc0d781601fb77dbdaa899acfd08ac560aec19f2d18bb3b6e25beb7a");
         var user_id = payload.Uid;
+        
+        // get programs
+        const program_date = await pool.query(`SELECT DISTINCT TO_CHAR(date, 'yyyy-mm-dd') AS start_date
+                                               FROM "WormGym".fitness_program
+                                               WHERE user_id = $1
+                                               ORDER BY start_date`, [user_id]);
+        const pd_list = program_date.rows;
+
+        for (let i = 0; i < pd_list.length; i++) {
+            // get the start date of the program
+            const sd = pd_list[i].start_date;
+
+            // get records of that week
+            const record = await pool.query(`SELECT equip_id, reps, sets, TO_CHAR(date, 'yyyy-mm-dd') AS r_date
+                                         FROM "WormGym".fitness_record
+                                         WHERE user_id = $1 AND ("date" BETWEEN $2 AND $2 + 6)
+                                         ORDER BY r_date`, [user_id, sd]);
+            let record_list = record.rows;
+            if (record_list.length == 0) {
+                //console.log('No record this week.');
+                continue;
+            }
+
+            // set day of week
+            let r_date_value = record_list[0].r_date;
+            let day_value = 1;
+            for (let j = 0; j < record_list.length; j++) {
+                let r = record_list[j];
+                if (r.r_date != r_date_value) {
+                    day_value += 1;
+                    r_date_value = r.r_date;
+                }
+                r.day = day_value;
+                r.reps = r.reps.split(" ")[0];  // remove unit
+
+                // check status
+                let p_item = await pool.query(`SELECT equip_id, reps, sets
+                                               FROM "WormGym".fitness_program
+                                               WHERE user_id = $1 AND "date" = $2 AND "Day" = $3 AND equip_id = $4`, [user_id, sd, r.day, r.equip_id]);
+                p_item = p_item.rows
+                if (p_item.length > 0) {
+                    p_item = p_item[0];
+                    p_item.reps = p_item.reps.split(" ")[0];  // remove unit
+                    if (r.reps >= p_item.reps && r.sets >= p_item.sets) {
+                        await pool.query(`UPDATE "WormGym".fitness_program SET finish=true
+                                          WHERE user_id = $1 AND "date" = $2 AND "Day" = $3 AND equip_id = $4`, [user_id, sd, r.day, r.equip_id]);
+                    }
+                }
+            }
+        }
+        
 
         // get all month
         const month = await pool.query(`SELECT EXTRACT(YEAR FROM month_trunc) AS year, EXTRACT(MONTH FROM month_trunc) as month
@@ -17,7 +68,8 @@ router.get('/', async (req, res) => {
                                             SELECT date_trunc('month', date) AS month_trunc FROM "WormGym".fitness_program
                                             WHERE user_id = $1
                                             GROUP BY month_trunc
-                                        ) AS mt`, [user_id]);
+                                        ) AS mt
+                                        ORDER BY year, month`, [user_id]);
         let month_list = month.rows;
 
         // calculate finish rate of each month
