af5bc06c5804fb7c940824691cac6bfd040123b4
liangyuchen <cy9815526@gmail.com>
2022-05-15 00:14:02 +0800

    Update menu API

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -4,7 +4,7 @@ const pool = require("../db");
 var jwt = require("jsonwebtoken");
 const e = require('express');
 
-router.get('/:time', async function (req, res) {
+router.get('/:time', function (req, res) {
     //Parameters
 	var JWT = req.headers.authorization
     var payload = jwt.verify(JWT, "b7b16ad9db0ca7c5705cba37840e4ec310740c62beea61cfd9bdcee0720797a6c8bb1b3ffc0d781601fb77dbdaa899acfd08ac560aec19f2d18bb3b6e25beb7a");
