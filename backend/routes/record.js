e0648681001066cf160eb3cd79e26fc42eda6d41
judycpc <abc0952753111@gmail.com>
2022-05-14 23:09:10 +0800

    finish status update move to record api

--- backend/routes/record.js
+++ backend/routes/record.js
@@ -8,12 +8,79 @@ router.post('/', async (req, res) => {
 	var payload = jwt.verify(JWT, "b7b16ad9db0ca7c5705cba37840e4ec310740c62beea61cfd9bdcee0720797a6c8bb1b3ffc0d781601fb77dbdaa899acfd08ac560aec19f2d18bb3b6e25beb7a");
 	var user_id = payload.Uid;
 	const {equip_id, weight, reps, sets, date} = req.body;
+	const input_date = new Date(date);
 
-	//Insert record into database
+	// Insert record into database
 	await pool.query(
 		'INSERT INTO "WormGym"."fitness_record" (user_id, equip_id, weight, reps, sets, date) VALUES($1, $2, $3, $4, $5, $6)'
 		, [user_id, equip_id, weight, reps, sets, date]
 	);
+
+	// Update finish status
+	// get start dates of programs
+	const program_date = await pool.query(`SELECT DISTINCT TO_CHAR(date, 'yyyy-mm-dd') AS start_date
+										   FROM "WormGym".fitness_program
+										   WHERE user_id = $1
+										   ORDER BY start_date`, [user_id]);
+	const pd_list = program_date.rows;
+
+	// get the start date of the program according to input
+	let sd = undefined;
+	for (let i = 0; i < pd_list.length; i++) {
+		const pd = new Date(pd_list[i].start_date);
+		const pd_end = new Date(pd_list[i].start_date);
+		pd_end.setDate(pd_end.getDate() + 6);
+		
+		if (input_date >= pd && input_date <= pd_end) {
+			sd = pd;
+			console.log(sd);
+			break;
+		}
+	}
+
+	// if the program exists
+	if (sd !== undefined) {
+		// get records of that week
+		const record = await pool.query(`SELECT equip_id, reps, sets, TO_CHAR(date, 'yyyy-mm-dd') AS r_date
+										 FROM "WormGym".fitness_record
+										 WHERE user_id = $1 AND ("date" BETWEEN $2 AND $2 + 6)
+										 ORDER BY r_date`, [user_id, sd]);
+		let record_list = record.rows;
+		console.log(record_list);
+
+		// set day of week
+		let r_date_value = record_list[0].r_date;
+		let day_value = 1;
+		let input_day = 0;
+		for (let j = 0; j < record_list.length; j++) {
+			let r = record_list[j];
+			if (r.r_date != r_date_value) {
+				day_value += 1;
+				r_date_value = r.r_date;
+			}
+			r.day = day_value;
+
+			if (input_date.toISOString().substring(0, 10) === r.r_date) {
+				input_day = r.day;
+			}
+
+			r.reps = r.reps.split(" ")[0];  // remove unit
+		}
+		
+		// check & update status
+		let p_item = await pool.query(`SELECT equip_id, reps, sets
+									   FROM "WormGym".fitness_program
+									   WHERE user_id = $1 AND "date" = $2 AND "Day" = $3 AND equip_id = $4`, [user_id, sd, input_day, equip_id]);
+		p_item = p_item.rows
+		if (p_item.length > 0) {
+			p_item = p_item[0];
+			p_item.reps = p_item.reps.split(" ")[0];  // remove unit
+			if (reps >= p_item.reps && sets >= p_item.sets) {
+				await pool.query(`UPDATE "WormGym".fitness_program SET finish=true
+								  WHERE user_id = $1 AND "date" = $2 AND "Day" = $3 AND equip_id = $4`, [user_id, sd, input_day, equip_id]);
+			}
+		}
+	}
 
 	//Send status to frontend
 	res.status(200).send('Success');
